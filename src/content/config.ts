23b4b906ebe32d76e25d69bef0b548db3ea96bbb
yikeguo <isjianqiang@qq.com>
2023-02-08 11:06:32 +0800

    Content Collections

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from 'astro:content';
+// 2. Define a schema for each collection you'd like to validate.
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    tag: z.array(z.string()),
+    pubDate: z.string(),
+    draft: z.boolean().default(false),
+    blogs: z.array(z.object({link: z.string(), from: z.string()})).default([]),
+    description: z.string().default('')
+  }),
+});
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  'blog': blogCollection,
+};
