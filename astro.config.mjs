cd6d3397491355038553745e239181da79583cf1
扉漆 <isjianqiang@qq.com>
2023-08-15 13:38:16 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yikeguo.github.io',
-  baseUrl: '/my-astro-site',
+  baseUrl: '/about',
   integrations: [mdx({
     rehypePlugins: [rehypeKatex]
   }), sitemap(), vue()],
